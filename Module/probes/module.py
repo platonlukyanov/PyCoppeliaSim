525c34dbbf57daea55165bd0313f5b20993b9b79
platonlukyanov <76910837+platonlukyanov@users.noreply.github.com>
2021-02-19 19:53:46 +0300

    Set_position was added

--- Module/probes/module.py
+++ Module/probes/module.py
@@ -1,5 +1,5 @@
 import b0RemoteApi
-
+import time
 def simulationStepStarted(msg):
 	simTime = msg[1][b'simulationTime']
 	print('Simulation step started. Simulation time: ', simTime)
@@ -12,14 +12,16 @@ def simulationStepDone(msg):
 
 class Coppelia:
 	doNextStep = True
-	def __init__(self, stepStartedFunc=simulationStepStarted, stepDoneFunc=simulationStepDone):
-
-		self.client = b0RemoteApi.RemoteApiClient('b0RemoteApi_pythonClient', 'b0RemoteApi')
+	def __init__(self, stepStartedFunc=simulationStepStarted, stepDoneFunc=simulationStepDone, api_name="b0RemoteApi"):
+		self.client = b0RemoteApi.RemoteApiClient('b0RemoteApi_pythonClient', api_name)
 		self.client.simxSynchronous(True)
 		self.client.simxGetSimulationStepStarted(self.client.simxDefaultSubscriber(stepStartedFunc))
 		self.client.simxGetSimulationStepDone(self.client.simxDefaultSubscriber(stepDoneFunc))
 		self.client.simxStartSimulation(self.client.simxDefaultPublisher()) 
-	
+
+	def set_position(self, object_name, position):
+		link = self.client.simxGetObjectHandle(object_name, self.client.simxServiceCall())
+		self.client.simxSetJointTargetPosition(link[1], position, self.client.simxServiceCall())
 
 	def stop_sim(self):
 		self.client.simxStopSimulation(self.client.simxDefaultPublisher())
@@ -27,7 +29,4 @@ class Coppelia:
 	def __del__(self):
 		self.stop_sim()
 		del self.client
-if __name__ == '__main__':
-	copp = Coppelia()
-	print(copp.client)
-	del copp
+
--- Module/probes/test.py
+++ Module/probes/test.py
@@ -1,2 +1,6 @@
-
-print(copp.client)
+from module import Coppelia
+import math
+copp = Coppelia(api_name="b0RemoteApi1")
+client = copp.client
+while True:
+	copp.set_position('Rev74', 120*180/math.pi)
